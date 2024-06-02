b236419c79c28a5c749fecf4dc14eaafce92ab42
kang yi fan <jason3631061@gmail.com>
2024-06-02 01:02:03 -0700

    Add prompt feature

--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -1,52 +1,51 @@
-'use client';
-import {useState} from 'react'
-import {useSession} from 'next-auth/react'
-import {userRouter} from 'next/navigation'
+"use client";
 
-import Form from '@components/Form'
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-    
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: '',
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    })
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-    const createPrompt = async (e) => {
-        e.preventDefault();
-        setIsSubmitting(true);
-    
-        try {
-          const response = await fetch("/api/prompt/new", {
-            method: "POST",
-            body: JSON.stringify({
-              prompt: post.prompt,
-              userId: session?.user.id,
-              tag: post.tag,
-            }),
-          });
-    
-          if (response.ok) {
-            router.push("/");
-          }
-        } catch (error) {
-          console.log(error);
-        } finally {
-          setIsSubmitting(false);
-        }
-      };
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
-    return (
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
     <Form
-    type="Create"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={createPrompt}
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
     />
-  )
-}
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
